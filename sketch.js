67c7bca4cdfb2744e1b208a1412333c9410b8c74
hwang30git <huiwang7265@gmail.com>
2022-07-22 20:33:25 -0400

    added end game on a full board with no winner

--- sketch.js
+++ sketch.js
@@ -116,6 +116,20 @@ function endGame() {
         winner = board[0][0];
         gameState = "end";
     }
+
+    let count = 0;
+    
+    for (let row = 0; row < board.length; row++) {
+        for (let col = 0; col < board[row].length; col++) {
+            if (board[row][col] == " ") {
+                count++;
+            }
+        }
+    }
+
+    if (count == 0) {
+        gameState = "end";
+    }
 }
 
 function mouseClicked() {
